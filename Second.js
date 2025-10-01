a53eeea523e000838b634eaa0a427127a039cfc1
Chand Zaib <chandcza786@gmail.com>
2025-10-01 11:37:22 +0500

    Second

---
+++ Second.js
@@ -0,0 +1,17 @@
+"use strict" //treat all code as a newer version
+
+
+//alert(3+3) we'r using node.js not browser
+//code readability should be high
+//number => to power 53
+//bignit
+//string => ""
+//boolean= true/flase
+//null = stand alone value
+//undefined => not assigned
+//symbol => uniqueness
+
+
+//object
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
